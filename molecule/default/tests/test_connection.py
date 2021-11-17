c4e759ba54bc0aca5803f414b617df4d389360b3
George Shuklin <george.shuklin@gmail.com>
2021-11-17 21:04:43 +0200

    feat: innernet playbooks

---
+++ molecule/default/tests/test_connection.py
@@ -0,0 +1,20 @@
+import pytest
+
+testinfra_hosts = ['ansible://innernet_members?force_ansible=True']
+
+@pytest.fixture()
+def peers(host):
+    ips = []
+    inventory = host.backend.ansible_runner.inventory
+    inventory_hostname = host.backend.get_hostname()
+    other_hosts = set(inventory['innernet_members']['hosts']) - set([inventory_hostname])
+    for other in other_hosts:
+        ips.append(inventory['_meta']['hostvars'][other]['innernet_ip'])
+    return ips
+    
+
+def test_ok(host, peers):
+    for peer in peers:
+        res = host.run(f'ping -c 2 {peer}')
+        if res.rc:
+            pytest.fail(res.stderr)
